e08a0824351e70ff6b72e8ccaf110bc038f26b0a
Jak Huyen <jakhuyen@yahoo.com>
2020-01-06 18:44:26 -0700

    Added localparam to clean up variable vector sizing and started coding loop/countdown functionality.

--- verilog/src/counter.v
+++ verilog/src/counter.v
@@ -1,33 +1,56 @@
 module counter #(
-    parameter integer MAX_CNT, 
+    parameter integer MAX_CNT = 2, 
     parameter LOOP = 1'b1,
-    localparam integer C_CNT_ARR_SIZE = ($log10(MAX_CNT)/$log10(2))
+    parameter IS_CNT_DOWN = 1'b0,
+    localparam integer C_CNT_ARR_SIZE = IS_CNT_DOWN?($floor($log10(MAX_CNT)/$log10(2)+1)):$floor($log10(MAX_CNT)/$log10(2))
 )
 (
     input i_clk,
     input i_rst,
+    input i_ctrl,
     output o_cnt_done,
-    output [($log10(MAX_CNT)/$log10(2)):0] o_cnt_val
+    output [C_CNT_ARR_SIZE:0] o_cnt_val
 );
 
-    reg [($log10(MAX_CNT)/$log10(2)):0] r_cnt_val = 0;
-    reg r_cnt_done = 0;
+    // When a parameter is dependent on another parameter, it seems like it will be evaluated last.
+    reg [C_CNT_ARR_SIZE:0] r_cnt_val = 0;
+    reg r_cnt_done;
+    reg r_ctrl;
 
-    // Wire Assignments
-    assign o_cnt_val  = r_cnt_val;
     assign o_cnt_done = r_cnt_done;
+    assign o_cnt_val  = r_cnt_val;
 
     always @ (posedge i_rst, posedge i_clk) begin
         if (i_rst == 1) begin
-            r_cnt_val <= 0;
+            r_cnt_val  <= 0;
+            r_cnt_done <= 0;
+            r_ctrl     <= 0;
         end
 
         else if (i_clk == 1) begin
             r_cnt_done <= 0;
-            r_cnt_val  <= r_cnt_val + 1;
 
-            if (r_cnt_val == MAX_CNT) begin
-                r_cnt_done <= 1;
+            if (r_ctrl == 0 && i_ctrl == 1) begin
+                r_ctrl <= 1;
+            end
+
+            else if (r_ctrl == 1 && i_ctrl == 1) begin
+                r_ctrl <= 0;
+            end
+
+            if (IS_CNT_DOWN == 0) begin
+                r_cnt_val <= r_cnt_val + 1;
+
+                if (r_cnt_val == MAX_CNT) begin
+                    r_cnt_done <= 1;
+                    r_cnt_val  <= 0;
+                end
+            end
+
+            else begin
+                r_cnt_val <= r_cnt_val - 1;
+
+                //if (r_cnt_val[] == )
             end
         end
     end
