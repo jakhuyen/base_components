202899c3dbab796a2ccdc4ba129b8880716753f9
Jak Huyen <jakhuyen@yahoo.com>
2020-01-06 18:41:53 -0700

    Added counter to clk_divider module

--- verilog/src/clk_divider.v
+++ verilog/src/clk_divider.v
@@ -1,8 +1,19 @@
-module clk_divider #(parameter div_num = 3) (
+module clk_divider #(parameter DIV_NUM = 3) (
     input i_clk,
+    input i_rst,
     output o_clk
 );
 
-    assign o_clk = i_clk;
+    counter #(.MAX_CNT(DIV_NUM), .LOOP(1)) cnt_div
+    (
+        .i_clk(i_clk),
+        .i_rst(i_rst),
+        .o_cnt_done(r_cnt_done),
+        .o_cnt_val(r_cnt_out)
+    );
+
+    always @ (posedge i_rst, posedge i_clk) begin
+        if (i_rst == 1) begin
+            
 
 endmodule
