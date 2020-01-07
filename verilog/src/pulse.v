8be79219c1eb47c0e27a102ab4e65ea8283e6d4b
Jak Huyen <jakhuyen@yahoo.com>
2020-01-06 18:46:13 -0700

    Completed coding of pulse module. Will output a one clock cycle pulse when it detects a rising edge at the input port.

--- verilog/src/pulse.v
+++ verilog/src/pulse.v
@@ -2,19 +2,21 @@ module pulse (
     input i_clk,
     input i_rst,
     input i_sig,
-    output reg o_pulse);
+    output o_pulse);
 
-    // TODO: If i_sig goes high, output a single pulse out of o_pulse.
-    // However, only do that on a rising edge of i_sig.
-
+    reg r_sig;
 
     always @ (posedge i_rst, posedge i_clk) begin
         if (i_rst == 1) begin
-
+            r_sig <= 0;
         end
 
         else if (i_clk == 1) begin
+            r_sig <= 0;
 
+            if (posedge i_sig) begin
+                r_sig <= 1;
+            end
         end
     end
 endmodule
