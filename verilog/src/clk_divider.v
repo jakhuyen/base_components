260d50d4572a6d8ccba1d6a1e8957cad3a63cefb
Jak Huyen <jakhuyen@yahoo.com>
2020-01-12 16:22:07 -0700

    Changed clk_divider indent from 4 spaces to 2. Finished initial coding of clk_divider.

--- verilog/src/clk_divider.v
+++ verilog/src/clk_divider.v
@@ -1,19 +1,33 @@
-module clk_divider #(parameter DIV_NUM = 3) (
-    input i_clk,
-    input i_rst,
-    output o_clk
+module clk_divider #(parameter DIV_NUM = 4) (
+  input i_clk,
+  input i_rst,
+  output o_clk
 );
 
-    counter #(.MAX_CNT(DIV_NUM), .LOOP(1)) cnt_div
-    (
-        .i_clk(i_clk),
-        .i_rst(i_rst),
-        .o_cnt_done(r_cnt_done),
-        .o_cnt_val(r_cnt_out)
-    );
+  reg r_clk;
 
-    always @ (posedge i_rst, posedge i_clk) begin
-        if (i_rst == 1) begin
-            
+  wire w_cnt_done;
 
+  assign o_clk = r_clk;
+
+  counter #(.MAX_CNT(DIV_NUM), .LOOP(1)) cnt_div
+  (
+    .i_clk(i_clk),
+    .i_rst(i_rst),
+    .o_cnt_done(w_cnt_done),
+    .o_cnt_val()
+  );
+
+  always @ (posedge i_rst, posedge i_clk) begin
+    if (i_rst == 1) begin
+      r_cnt_done <= 0;
+      r_clk      <= 0;
+    end
+
+    else if (i_clk == 1) begin
+      if (w_cnt_done == 1) begin
+        r_clk <= ~r_clk;
+      end
+    end
+  end
 endmodule
