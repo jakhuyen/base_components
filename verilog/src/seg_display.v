f50216d8590d2c0f994cc18aec0128a511260705
Jak Huyen <jakhuyen@yahoo.com>
2020-01-12 08:24:35 -0700

    Changed seg_display indent from 4 spaces to 2.

--- verilog/src/seg_display.v
+++ verilog/src/seg_display.v
@@ -6,44 +6,42 @@ module seg_display (
     output reg o_decimal
 );
 
-    // A is the LSB and G is the MSB
-    // 1's turn off the segment.
-    // ----A----
-    // |       |
-    // F       B
-    // |       |
-    // |---G---|
-    // |       |
-    // E       C
-    // |       |
-    // ----D----
+  // A is the LSB and G is the MSB
+  // 1's turn off the segment.
+  // ----A----
+  // |       |
+  // F       B
+  // |       |
+  // |---G---|
+  // |       |
+  // E       C
+  // |       |
+  // ----D----
+  
+  reg [6:0] r_seg   = {7{1'b1}};
+  reg [4:0] r_digit = {4{1'b1}};
 
-    reg [6:0] r_seg   = {7{1'b1}};
-    reg [4:0] r_digit = {4{1'b1}};
-
-    always @ (posedge i_rst, posedge i_clk) begin
-        if (i_rst == 1) begin
-            r_seg <= 7'hFF;
-        end
-
-        else if (i_clk == 1) begin
-            case (i_digit)
-                1 : r_seg <= 7'h79;
-                2 : r_seg <= 7'h24;
-                3 : r_seg <= 7'h30;
-                4 : r_seg <= 7'h19;
-                5 : r_seg <= 7'h12;
-                6 : r_seg <= 7'h02;
-                7 : r_seg <= 7'h78;
-                8 : r_seg <= 7'h00;
-                9 : r_seg <= 7'h10;
-                default : r_seg <= 7'hFF;
-            endcase
-
-            o_seg <= r_seg;
-            o_decimal <= 1'b1;
-        end
+  always @ (posedge i_rst, posedge i_clk) begin
+    if (i_rst == 1) begin
+      r_seg <= 7'hFF;
     end
 
-    
+    else if (i_clk == 1) begin
+      case (i_digit)
+        1 : r_seg <= 7'h79;
+        2 : r_seg <= 7'h24;
+        3 : r_seg <= 7'h30;
+        4 : r_seg <= 7'h19;
+        5 : r_seg <= 7'h12;
+        6 : r_seg <= 7'h02;
+        7 : r_seg <= 7'h78;
+        8 : r_seg <= 7'h00;
+        9 : r_seg <= 7'h10;
+        default : r_seg <= 7'hFF;
+      endcase
+
+      o_seg <= r_seg;
+      o_decimal <= 1'b1;
+    end
+  end  
 endmodule
