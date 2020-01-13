d443958007a72d71df587131680944366e49f612
Jak Huyen <jakhuyen@yahoo.com>
2020-01-12 18:58:57 -0700

    Finished enable feature of the counter.

--- verilog/src/counter.v
+++ verilog/src/counter.v
@@ -2,12 +2,12 @@ module counter #(
     parameter integer MAX_CNT = 2, 
     parameter LOOP = 1'b1,
     parameter IS_CNT_DOWN = 1'b0,
-    localparam integer C_CNT_ARR_SIZE = IS_CNT_DOWN?($floor($log10(MAX_CNT)/$log10(2)+1)):$floor($log10(MAX_CNT)/$log10(2))
+    localparam integer C_CNT_ARR_SIZE = IS_CNT_DOWN?$rtoi($floor($log10(MAX_CNT)/$log10(2)+1)):$rtoi($floor($log10(MAX_CNT)/$log10(2)))
 )
 (
     input i_clk,
     input i_rst,
-    input i_ctrl,
+    input i_en,
     output o_cnt_done,
     output [C_CNT_ARR_SIZE:0] o_cnt_val
 );
@@ -15,7 +15,7 @@ module counter #(
     // When a parameter is dependent on another parameter, it seems like it will be evaluated last.
     reg [C_CNT_ARR_SIZE:0] r_cnt_val = 0;
     reg r_cnt_done;
-    reg r_ctrl;
+    reg r_en;
 
     assign o_cnt_done = r_cnt_done;
     assign o_cnt_val  = r_cnt_val;
@@ -24,33 +24,30 @@ module counter #(
         if (i_rst == 1) begin
             r_cnt_val  <= 0;
             r_cnt_done <= 0;
-            r_ctrl     <= 0;
+            r_en       <= 0;
         end
 
         else if (i_clk == 1) begin
-            r_cnt_done <= 0;
+            if (i_en == 1) begin
+                r_cnt_done <= 0;
 
-            if (r_ctrl == 0 && i_ctrl == 1) begin
-                r_ctrl <= 1;
-            end
+                if (IS_CNT_DOWN == 0) begin
+                   r_cnt_val <= r_cnt_val + 1;
 
-            else if (r_ctrl == 1 && i_ctrl == 1) begin
-                r_ctrl <= 0;
-            end
-
-            if (IS_CNT_DOWN == 0) begin
-                r_cnt_val <= r_cnt_val + 1;
-
-                if (r_cnt_val == MAX_CNT) begin
-                    r_cnt_done <= 1;
-                    r_cnt_val  <= 0;
+                    if (r_cnt_val == MAX_CNT) begin
+                        r_cnt_done <= 1;
+                        r_cnt_val  <= 0;
+                    end
                 end
-            end
 
-            else begin
-                r_cnt_val <= r_cnt_val - 1;
+                else begin
+                    r_cnt_val <= r_cnt_val - 1;
 
-                //if (r_cnt_val[] == )
+                    if (r_cnt_val[C_CNT_ARR_SIZE] == 1) begin
+                        r_cnt_done <= 1;
+                        r_cnt_val  <= MAX_CNT;
+                    end
+                end
             end
         end
     end
