7e391b1643e525a53b0cc34d8c66a859884daea5
Jak Huyen <jakhuyen@yahoo.com>
2020-01-13 19:37:07 -0700

    Added missing comma to port list

--- verilog/src/DFF.v
+++ verilog/src/DFF.v
@@ -4,7 +4,7 @@ module DFF (
   input i_rst,
   input i_en,
   input i_d,
-  output reg o_q
+  output reg o_q,
   output reg o_q_not
 );
 
@@ -15,6 +15,8 @@ module DFF (
     end
 
     else if (i_clk == 1) begin
+      // If enable is asserted, both q and q_not will be assigned the appropriate input value.
+      // If enable is de-asserted, q and q_not will stay the same as before.
       if (i_en == 1) begin
         o_q     <= i_d;
         o_q_not <= ~i_d;
