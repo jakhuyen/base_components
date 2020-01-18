cab57b4beac9697b61d89d3bf7d2dfbadd4c0f17
Jak Huyen <jakhuyen@yahoo.com>
2020-01-17 19:00:40 -0700

    Updated DFF to adhere to updated coding standards

--- verilog/src/DFF.v
+++ verilog/src/DFF.v
@@ -1,25 +1,25 @@
 // Module for a D Flip-FLop with an enable control pin
 module DFF (
-  input i_clk,
-  input i_rst,
-  input i_en,
-  input i_d,
-  output reg o_q,
-  output reg o_q_not
+  input clkIn,
+  input rstIn,
+  input enIn,
+  input dIn,
+  output reg qOut,
+  output reg qNotOut
 );
 
-  always @(posedge i_rst, posedge i_clk) begin
-    if (i_rst == 1) begin
-      o_q     <= 0;
-      o_q_not <= 1;
+  always @(posedge rstIn, posedge clkIn) begin
+    if (rstIn == 1) begin
+      qOut    <= 0;
+      qNotOut <= 1;
     end
 
-    else if (i_clk == 1) begin
+    else if (clkIn == 1) begin
       // If enable is asserted, both q and q_not will be assigned the appropriate input value.
       // If enable is de-asserted, q and q_not will stay the same as before.
-      if (i_en == 1) begin
-        o_q     <= i_d;
-        o_q_not <= ~i_d;
+      if (enIn == 1) begin
+        qOut    <= dIn;
+        qNotOut <= ~dIn;
       end
     end
   end
