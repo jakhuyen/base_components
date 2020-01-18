3ee518c2e5de58abd6d903c4b50549e1c0b7781f
Jak Huyen <jakhuyen@yahoo.com>
2020-01-17 17:39:28 -0700

    Changed seg_display to adhere to updated coding standards

--- verilog/src/seg_display.v
+++ verilog/src/seg_display.v
@@ -1,9 +1,9 @@
-module seg_display (
-    input i_clk,
-    input i_rst,
-    input [4:0] i_digit,
-    output reg [6:0] o_seg,
-    output reg o_decimal
+module segDisplay (
+    input clkIn,
+    input rstIn,
+    input [4:0] digitIn,
+    output reg [6:0] segOut,
+    output reg decimalOut
 );
 
   // A is the LSB and G is the MSB
@@ -18,30 +18,30 @@ module seg_display (
   // |       |
   // ----D----
   
-  reg [6:0] r_seg   = {7{1'b1}};
-  reg [4:0] r_digit = {4{1'b1}};
+  reg [6:0] segR   = {7{1'b1}};
+  reg [4:0] digitR = {4{1'b1}};
 
-  always @ (posedge i_rst, posedge i_clk) begin
-    if (i_rst == 1) begin
-      r_seg <= 7'hFF;
+  always @ (posedge rstIn, posedge clkIn) begin
+    if (rstIn == 1) begin
+      segR <= 7'hFF;
     end
 
-    else if (i_clk == 1) begin
-      case (i_digit)
-        1 : r_seg <= 7'h79;
-        2 : r_seg <= 7'h24;
-        3 : r_seg <= 7'h30;
-        4 : r_seg <= 7'h19;
-        5 : r_seg <= 7'h12;
-        6 : r_seg <= 7'h02;
-        7 : r_seg <= 7'h78;
-        8 : r_seg <= 7'h00;
-        9 : r_seg <= 7'h10;
-        default : r_seg <= 7'hFF;
+    else if (clkIn == 1) begin
+      case (digitIn)
+        1 : segR <= 7'h79;
+        2 : segR <= 7'h24;
+        3 : segR <= 7'h30;
+        4 : segR <= 7'h19;
+        5 : segR <= 7'h12;
+        6 : segR <= 7'h02;
+        7 : segR <= 7'h78;
+        8 : segR <= 7'h00;
+        9 : segR <= 7'h10;
+        default : segR <= 7'hFF;
       endcase
 
-      o_seg <= r_seg;
-      o_decimal <= 1'b1;
+      segOut     <= r_seg;
+      decimalOut <= 1'b1;
     end
   end  
 endmodule
