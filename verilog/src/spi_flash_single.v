ce60e2fbbceee29bf1f821864938b252f389457c
jakhuyen <jakhuyen@yahoo.com>
2020-03-21 21:23:49 -0500

    Update spi_flash_single.v
    Finished initial code of SPI module

--- verilog/src/spi_flash_single.v
+++ verilog/src/spi_flash_single.v
@@ -14,14 +14,20 @@ module spi_flash_single #(
   input [DATA_WIDTH-1:0] txDataIn,
   output [DATA_WIDTH-1:0] rxDataOut,
   output rxRdyOut,
-  output mosiOut,
+  output reg mosiOut,
   output [NUM_SS-1:0] csLowOut,
   output sclkOut
 );
 
+  localparam reg[2:0] IDLE     = 3'b000;
+  localparam reg[2:0] GET_DATA = 3'b001;
+  localparam reg[2:0] LD_DATA  = 3'b010;
+  localparam reg[2:0] TR       = 3'b011;
+  localparam reg[2:0] PHA_DLY  = 3'b100;
+
   localparam DIV_NUM = 100000000/SCLK_FREQ;
 
-  integer txBitCntR, rxBitCntR;
+  integer txBitCntR, rxBitCntR, lastBitCntR;
 
   reg prevClkStateR;
   reg idleR;
@@ -29,19 +35,21 @@ module spi_flash_single #(
   reg getTxDataR;
   reg pushRxDataR;
   reg [DATA_WIDTH-1:0] rxDataR;
-  reg rxDoneR, txDoneR;
+  reg [2:0] spiFSMR;
 
-  wire txDataW, txFifoFullW, txFifoEmptyW;
+  wire [DATA_WIDTH-1:0] txDataW;
+  wire txFifoFullW, txFifoEmptyW;
   wire sclkW;
   wire sclkMainW  = (CPOL == 0)?sclkW:~sclkW;
   assign sclkOut  = sclkMainW;
   assign csLowOut = csLowR;
+  assign rxRdyOut = (rxFifoEmptyW == 1)?0:1;
 
   clk_divider #(.DIV_NUM(DIV_NUM)) sclk_inst
   (
     .clkIn(clkIn),
     .rstIn(rstIn & idleR),
-    .enIn(1'b1)
+    .enIn(1'b1),
     .clkOut(sclkR)
   );
 
@@ -73,46 +81,62 @@ module spi_flash_single #(
     if (rstIn == 1) begin
       prevClkStateR <= CPOL;
       idleR         <= 1;
-      csLowR        <= NUM_SS{1'b1};
+      csLowR        <= {NUM_SS{1'b1}};
       getTxDataR    <= 0;
       pushRxDataR   <= 0;
       txBitCntR     <= 0;
       rxBitCntR     <= 0;
-      rxDoneR       <= 0;
-      txDoneR       <= 0;
+      lastBitCntR   <= 0;
     end
 
     else if (clkIn == 1) begin
       prevClkStateR <= sclkMainW;
-      idleR         <= 0;
       getTxDataR    <= 0;
       pushRxDataR   <= 0;
 
       case (spiFSMR)
         IDLE:
           begin
-            txBitCntR <= 0;
-            rxBitCntR <= 0;
-            txDoneR   <= 0;
-            rxDoneR   <= 0;
-            csLowR    <= NUM_SS{1'b1};
+            txBitCntR   <= 0;
+            rxBitCntR   <= 0;
+            lastBitCntR <= 0;
+            idleR       <= 1;
+            csLowR      <= {NUM_SS{1'b1}};
 
             if (enIn == 1) begin
               csLowR[csIn]  <= 1'b0;
-              spiFSMR <= GET_DATA;
+              idleR         <= 0;
+              spiFSMR       <= GET_DATA;
             end
+          end
 
         GET_DATA:
           begin
             if (txFifoEmptyW == 0) begin
               getTxDataR <= 1;
-              spiFSMR    <= TR;
+              
+              // Need to preload data before first clock edge
+              if ((CPOL == 0 && CPHA == 0) || (CPOL == 1 && CPHA == 0)) begin
+                spiFSMR <= LD_DATA;
+              end
+              
+              // Data is sampled on the falling edge, so no need to preload data
+              else begin
+                spiFSMR <= TR;
+              end
             end
 
             // False start
             else begin
               spiFSMR <= IDLE;
             end
+          end
+            
+        LD_DATA:
+          begin
+            mosiOut   <= txDataW[0];
+            txBitCntR <= 1;
+          end
 
         TR:
           begin
@@ -122,61 +146,97 @@ module spi_flash_single #(
               // CPOL = 0, CPHA = 1 (Change Data)
               // CPOL = 1, CPHA = 0 (Change Data)
               // CPOL = 1, CPHA = 1 (Sample Data)
-              // Mode 0
-              if ((CPOL = 0 && CPHA == 0) || (CPOL = 1 && CPHA == 1)) begin
+              // Mode 0 and Mode 3
+              // If data is sampled on leading edge
+              if ((CPOL == 0 && CPHA == 0) || (CPOL == 1 && CPHA == 1)) begin                
                 if (rxBitCntR != DATA_WIDTH) begin
                   rxBitCntR <= rxBitCntR + 1;
                   rxDataR   <= {rxDataR[DATA_WIDTH-1:1], misoIn};
                 end
 
                 else if (rxBitCntR == DATA_WIDTH) begin
-                  if (misoIn == 1'bZ) begin
-                    rxDoneR <= 1;
-                  end
-
-                  else begin
-                    rxBitCntR   <= 1;
-                    rxDataR     <= misoIn;
-                  end
+                  pushRxDataR <= 1;
+                  rxBitCntR   <= 1;
+                  rxDataR     <= misoIn;
+                end
               end
 
+              // Mode 1 and Mode 2
               // If data changes on trailing edge
-              else if (CPHA == 1) begin
-                if 
+              else if ((CPOL == 0 && CPHA == 1) || (CPOL == 1 && CPHA == 0)) begin
+                if (txBitCntR != DATA_WIDTH) begin
+                  txBitCntR <= txBitCntR + 1;
+                  mosiOut   <= txDataW[txBitCntR];
+                end
+                
+                if (txBitCntR == DATA_WIDTH - 1) begin
+                  if (txFifoEmptyW != 1) begin
+                    getTxDataR <= 1;
+                    txBitCntR  <= 0;
+                  end
+                  
+                  else begin
+                    spiFSMR <= PHA_DLY;
+                  end
+                    
                 end
               end
             end
 
 
             // Detect falling edge
-            else if (prevClkStateR == 1 && sclkMainW == 0)
+            else if (prevClkStateR == 1 && sclkMainW == 0) begin
               // CPOL = 0, CPHA = 0 (Change Data)
               // CPOL = 0, CPHA = 1 (Sample Data)
               // CPOL = 1, CPHA = 0 (Sample Data)
               // CPOL = 1, CPHA = 1 (Change Data)
+              // Mode 1 and 2
               // If sampling data on falling edge
-              if (CPHA == 1 && misoIn != 1'bZ) begin
+              if ((CPOL == 0 && CPHA == 1) || (CPOL == 1 && CPHA == 0)) begin                
+                if (rxBitCntR != DATA_WIDTH) begin
+                  rxBitCntR <= rxBitCntR + 1;
+                  rxDataR   <= {rxDataR[DATA_WIDTH-1:1], misoIn};
+                end
 
+                else if (rxBitCntR == DATA_WIDTH) begin
+                  pushRxDataR <= 1;
+                  rxBitCntR   <= 1;
+                  rxDataR     <= misoIn;
+                end
               end
 
-              // If shifting data on falling edge
-              if (CPHA == 0) begin
-
+              // Mode 0 and 3
+              // If data changes on trailing edge
+              else if ((CPOL == 0 && CPHA == 0) || (CPOL == 1 && CPHA == 1)) begin
+                if (txBitCntR != DATA_WIDTH) begin
+                  txBitCntR <= txBitCntR + 1;
+                  mosiOut   <= txDataW[txBitCntR];
+                end
+                
+                if (txBitCntR == DATA_WIDTH - 1) begin
+                  if (txFifoEmptyW != 1) begin
+                    getTxDataR <= 1;
+                    txBitCntR  <= 0;
+                  end
+                  
+                  else begin
+                    spiFSMR <= PHA_DLY;
+                  end
+                end
               end
-            end
-
-            // If data is finished transmitting and there is more data
-            if (txBitCntR == DATA_WIDTH && txFifoEmptyW == 0) begin
-              getTxDataR <= 1;
             end
           end
 
-
-
-      prevClkStateR <= (CPHA == 0)?
-
-      // 
-
+        PHA_DLY:
+          begin
+            lastBitCntR <= lastBitCntR + 1;
+            if (lastBitCntR == DIV_NUM) begin
+              spiFSMR <= IDLE;
+            end
+          end
+      endcase      
+    end
+  end
 endmodule
 
 // Whenever enIn is asserted, txDataIn will be captured in a FIFO.
